31267cd96f2c16bde1f4fa87a0d066187c81bb0f
Scriptman 777 <dave56938@gmail.com>
2022-04-27 09:45:16 +0200

    Double graph prototype

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import socket
 from emoji import demojize
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import matplotlib.pyplot as plt
-import numpy as np
 from matplotlib.animation import FuncAnimation
 
 
@@ -10,7 +9,7 @@ server = 'irc.chat.twitch.tv'
 port = 6667
 nickname = 'VibeMeter'
 token = 'oauth:mg31dpbx7ggtbcde3cvxaqbe7zufpc'
-channel = '#nl_kripp'
+channel = '#tarik'
 
 sock = socket.socket()
 sock.connect((server, port))
@@ -19,14 +18,16 @@ sock.send(f"NICK {nickname}\r\n".encode('utf-8'))
 sock.send(f"JOIN {channel}\r\n".encode('utf-8'))
 
 ANALYZER = SentimentIntensityAnalyzer()
-overall_sentiment = {'compound': 0, 'neg': 0, 'neu': 0, 'pos': 0}
-recent_sentiment = {'compound': 0, 'neg': 0, 'neu': 0, 'pos': 0}
+overall_sentiment = {'compound': 0, 'neg': 0.1, 'neu': 0, 'pos': 0.1, 'total': 0}
 
 # Init graph
-fig = plt.figure(figsize=(12, 6))
-ax = fig.add_subplot(111)
-ax.bar(['Positive', 'Negative', 'Neutral'], [0, 0, 0], color=['green', 'red', 'grey'])
-ax.set_title('VibeMeter')
+fig, axs = plt.subplots(2,1,figsize=(6, 10))
+fig.tight_layout(pad=3.0)
+axs[0].bar(['Positive', 'Negative', 'Neutral'], [0, 0, 0], color=['green', 'red', 'grey'])
+axs[0].set_title('VibeMeter')
+axs[0].set_ylabel('Relative vibe')
+axs[1].pie([0.5, 0.5], labels=['Positive', 'Negative'], colors=['green', 'red'])
+
 
 
 def udpate_graph(arg):
@@ -47,11 +48,14 @@ def udpate_graph(arg):
             overall_sentiment['neg'] += sentiment_dict['neg']
             overall_sentiment['neu'] += sentiment_dict['neu']
             overall_sentiment['pos'] += sentiment_dict['pos']
+            overall_sentiment['total'] = overall_sentiment['neg'] + overall_sentiment['pos']
             print(overall_sentiment)
 
-    ax.cla()
-    ax.bar(['Positive', 'Negative', 'Neutral'], [overall_sentiment['pos'], overall_sentiment['neg'], overall_sentiment['neu']], color=['green', 'red', 'grey'])
-    ax.set_title('VibeMeter')
+    axs[0].cla()
+    axs[1].cla()
+    axs[0].bar(['Positive', 'Negative'], [overall_sentiment['pos']/overall_sentiment['total'], overall_sentiment['neg']/overall_sentiment['total']], color=['green', 'red'])
+    axs[1].pie([overall_sentiment['pos']/overall_sentiment['total'], overall_sentiment['neg']/overall_sentiment['total']], labels=['Positive', 'Negative'], colors=['green', 'red'])
+    axs[0].set_title('VibeMeter')
 
 def main():
     ani = FuncAnimation(fig, udpate_graph, interval=300)
