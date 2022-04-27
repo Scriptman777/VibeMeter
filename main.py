9db21d2781809e4bd47c8482001cce47a9ec3e8b
Scriptman 777 <dave56938@gmail.com>
2022-04-27 09:28:34 +0200

    Working prototype

--- main.py
+++ main.py
@@ -1,2 +1,61 @@
+import socket
+from emoji import demojize
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+
+
+server = 'irc.chat.twitch.tv'
+port = 6667
+nickname = 'VibeMeter'
+token = 'oauth:mg31dpbx7ggtbcde3cvxaqbe7zufpc'
+channel = '#nl_kripp'
+
+sock = socket.socket()
+sock.connect((server, port))
+sock.send(f"PASS {token}\r\n".encode('utf-8'))
+sock.send(f"NICK {nickname}\r\n".encode('utf-8'))
+sock.send(f"JOIN {channel}\r\n".encode('utf-8'))
+
+ANALYZER = SentimentIntensityAnalyzer()
+overall_sentiment = {'compound': 0, 'neg': 0, 'neu': 0, 'pos': 0}
+recent_sentiment = {'compound': 0, 'neg': 0, 'neu': 0, 'pos': 0}
+
+# Init graph
+fig = plt.figure(figsize=(12, 6))
+ax = fig.add_subplot(111)
+ax.bar(['Positive', 'Negative', 'Neutral'], [0, 0, 0], color=['green', 'red', 'grey'])
+ax.set_title('VibeMeter')
+
+
+def udpate_graph(arg):
+    resp = sock.recv(2048).decode('utf-8')
+
+    if resp.startswith('PING'):
+        sock.send("PONG\n".encode('utf-8'))
+    elif len(resp) > 0:
+        if ':streamlabs' not in resp:
+            demojized = demojize(resp)
+            cut = demojized.split(':')
+            sentiment_dict = ANALYZER.polarity_scores(cut[-1])
+            print(cut[-1])
+            print('===============================')
+            print(sentiment_dict)
+            print('===============================')
+            overall_sentiment['compound'] += sentiment_dict['compound']
+            overall_sentiment['neg'] += sentiment_dict['neg']
+            overall_sentiment['neu'] += sentiment_dict['neu']
+            overall_sentiment['pos'] += sentiment_dict['pos']
+            print(overall_sentiment)
+
+    ax.cla()
+    ax.bar(['Positive', 'Negative', 'Neutral'], [overall_sentiment['pos'], overall_sentiment['neg'], overall_sentiment['neu']], color=['green', 'red', 'grey'])
+    ax.set_title('VibeMeter')
+
+def main():
+    ani = FuncAnimation(fig, udpate_graph, interval=300)
+    plt.show()
+
 if __name__ == '__main__':
-    print('main.py')
+    main()
